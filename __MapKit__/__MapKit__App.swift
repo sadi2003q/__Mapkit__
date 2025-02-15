42d629193100b6ef8c880fdf0fe931bc675d9970
Md. Adnan Abdullah <Adnanabdullah625@gmail.com>
2025-02-15 17:28:38 +0600

    Initial Commit

---
+++ __MapKit__/__MapKit__App.swift
@@ -0,0 +1,17 @@
+//
+//  __MapKit__App.swift
+//  __MapKit__
+//
+//  Created by  Sadi on 15/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct __MapKit__App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
