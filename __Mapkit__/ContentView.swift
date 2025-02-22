3197f4ef19e04dc4b5082e48fdff2fb9f40826c0
Md. Adnan Abdullah <Adnanabdullah625@gmail.com>
2025-02-22 17:44:40 +0600

    Initial Commit

---
+++ __Mapkit__/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  __Mapkit__
+//
+//  Created by  Sadi on 22/02/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ __Mapkit__/__Mapkit__App.swift
@@ -0,0 +1,17 @@
+//
+//  __Mapkit__App.swift
+//  __Mapkit__
+//
+//  Created by  Sadi on 22/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct __Mapkit__App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
